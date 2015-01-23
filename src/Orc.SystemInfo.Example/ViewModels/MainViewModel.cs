57b651936956a0cc041f434779bfd78dbd5edc71
Maxim Khomutov <mkhomutov@gmail.com>
2015-01-23 23:23:06 +0300

    ORCOMP-51: Added simple example

---
+++ src/Orc.SystemInfo.Example/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainViewModel.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.SystemInfo.Example.ViewModels
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Catel.MVVM;
+    using Services;
+
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ISystemInfoService _systemInfoService;
+
+        public MainViewModel(ISystemInfoService systemInfoService)
+        {
+            _systemInfoService = systemInfoService;
+            ShowSystemInfo = new TaskCommand(OnShowSystemInfoExecute);
+        }
+
+        public TaskCommand ShowSystemInfo { get; private set; }
+
+        private async Task OnShowSystemInfoExecute()
+        {
+            var sysInfoLines = _systemInfoService.GetSystemInfo().Select(x => string.Format("{0} {1}", x.Value1, x.Value2));
+            SystemInfo = String.Join("\n", sysInfoLines);
+        }
+
+        public string SystemInfo { get; set; }
+    }
+}
