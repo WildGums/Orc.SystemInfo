2325987898a8966d976fee061be4f1972fac51f2
Geert van Horrik <geert@catenalogic.com>
2016-02-05 18:20:03 +0100

    Solution maintenance

--- src/Orc.SystemInfo/Orc.SystemInfo.Shared/Extensions/ManagementBaseObjectExtensions.cs
+++ src/Orc.SystemInfo/Orc.SystemInfo.Shared/Extensions/ManagementBaseObjectExtensions.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="ManagementObjectExtensions.cs" company="Wild Gums">
-//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// <copyright file="ManagementObjectExtensions.cs" company="WildGums">
+//   Copyright (c) 2008 - 2015 WildGums. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
