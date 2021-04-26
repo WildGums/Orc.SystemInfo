51b247474ce53e320befb250dd3ad99fd75529ef
Artem Saikin <art.saikin@gmail.com>
2021-04-26 20:49:51 +0300

    ComImport wbem interfaces

---
+++ src/Orc.SystemInfo/Win32/Wbem/Extensions/IWbemClassObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Orc.SystemInfo.Win32
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal static class IWbemClassObjectExtensions
+    {
+        internal static IEnumerable<string> GetNames(this IWbemClassObject wbemClassObject, WbemConditionFlag flags = WbemConditionFlag.WBEM_FLAG_NONSYSTEM_ONLY)
+        {
+            HResult hr = wbemClassObject.GetNames(null, flags, null, out string[] names);
+
+            if (hr.Failed)
+            {
+                throw (Exception)hr;
+            }
+
+            return names;
+        }
+
+        internal static object Get(this IWbemClassObject wbemClassObjecthis, string propertyName)
+        {
+            HResult hresult = wbemClassObjecthis.Get(propertyName, 0, out object value, out CimType type, out int flavor);
+
+            if (hresult.Failed)
+            {
+                throw (Exception)hresult;
+            }
+
+            return value == DBNull.Value ? null : value;
+        }
+    }
+}
---
+++ src/Orc.SystemInfo/Win32/Wbem/Interfaces/IWbemCallResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Orc.SystemInfo.Win32
+{
+    using System.Runtime.InteropServices;
+
+    [ComImport]
+    [Guid("44aca675-e8fc-11d0-a07c-00c04fb68820")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IWbemCallResult
+    {
+        [PreserveSig]
+        HResult GetResultObject([In] int timeout, [Out] out IWbemClassObject resultObject);
+
+        HResult GetResultString([In] int timeout, [Out, MarshalAs(UnmanagedType.BStr)] out string resultString);
+
+        [PreserveSig]
+        HResult GetResultServices([In] int timeout, [Out] out IWbemServices services);
+
+        [PreserveSig]
+        HResult GetCallStatus([In] int timeout, [Out] out HResult status);
+    }
+}
