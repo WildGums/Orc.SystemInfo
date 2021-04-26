38270b86311b738fb696890a42ea133791bcf62f
Artem Saikin <art.saikin@gmail.com>
2021-04-26 11:50:29 +0300

    Add CoSetProxyBlanket default shimmer and utility which wraps calls to it

--- src/Orc.SystemInfo/Win32/WmiNetUtils.cs
+++ src/Orc.SystemInfo/Win32/WmiNetUtils.cs
@@ -1,19 +1,18 @@
 ﻿namespace Orc.SystemInfo.Win32
 {
     using System;
-    using System.IO;
     using System.Runtime.InteropServices;
 
     /// <summary>
     /// This class wraps calls of CoSetProxyBlanket from managed shim library.
     /// CoSetProxyBlanket and CoInitializeSecurity both work to set the permissions context in which the process invoked immediately after is executed. 
     /// Calling them from within that process is useless because it's too late at that point; the permissions context has already been set.
-    /// Specifically, these methods are meant to be called from non-managed code such as a C++ wrapper that then invokes the managed, i.e.C# or VB.NET, code.
+    /// Specifically, these methods are meant to be called from non-managed code such as a C++ wrapper that then invokes the managed, i.e. C# or VB.NET, code.
     /// For more information, see <see url="https://docs.microsoft.com/en-us/archive/blogs/mbend/cosetproxyblanket-not-supported-from-managed-code"/>
     /// <see url="https://docs.microsoft.com/en-us/windows/win32/wmisdk/setting-the-security-levels-on-a-wmi-connection"/>.
     /// <see url="https://docs.microsoft.com/en-us/windows/win32/wmisdk/setting-client-application-process-security"/>
     /// </summary>
-    public static class WmiNetUtils
+    internal static class WmiNetUtils
     {
         /// <summary>
         /// Sets the authentication information that will be used to make calls on the specified proxy.
@@ -22,7 +21,8 @@
 
         static WmiNetUtils()
         {
-            string dllPath = @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\\wminet_utils.dll";
+            // string dllPath = @"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\\wminet_utils.dll";
+            string dllPath = @"C:\Dev\Orc.SystemInfo\src\Orc.SystemInfo\bin\WbemShim.dll";
 
             IntPtr loadLibrary = Kernel32.LoadLibrary(dllPath);
 
@@ -34,20 +34,27 @@
             }
             else
             {
-                throw new TypeInitializationException("wminet_utils.net (native)", null);
+                var error = Marshal.GetLastWin32Error();
+                throw new COMException($"Error calling native library", error);
             }
         }
 
-        // Set Authentication for wbem services
+        /// <summary>
+        /// Gets pointer to function setting proxy authorization on wbem service
+        /// </summary>
+        /// <param name="wbemServices"></param>
+        /// <param name="impersonationLevel"></param>
+        /// <param name="authenticationLevel"></param>
+        /// <returns></returns>
         internal delegate HResult CoSetProxyBlanketForIWbemServicesFunction(
             [In, MarshalAs(UnmanagedType.Interface)]
             IWbemServices wbemServices,
-            [In, MarshalAs(UnmanagedType.BStr)]
-            string userName,
-            [In, MarshalAs(UnmanagedType.BStr)]
-            string password,
-            [In, MarshalAs(UnmanagedType.BStr)]
-            string authority,
+            //[In, MarshalAs(UnmanagedType.BStr)]
+            //string userName,
+            //[In, MarshalAs(UnmanagedType.BStr)]
+            //string password,
+            //[In, MarshalAs(UnmanagedType.BStr)]
+            //string authority,
             [In]
             ImpersonationLevel impersonationLevel,
             [In]
