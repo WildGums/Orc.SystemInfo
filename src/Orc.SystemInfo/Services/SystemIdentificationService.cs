bb408bfe20897ae1662fe650f5cad25c62016aa0
Geert van Horrik <geert@catenalogic.com>
2021-11-18 12:49:56 +0100

    Add crypto inside usings

--- src/Orc.SystemInfo/Services/SystemIdentificationService.cs
+++ src/Orc.SystemInfo/Services/SystemIdentificationService.cs
@@ -188,17 +188,20 @@ namespace Orc.SystemInfo
 
         protected static string CalculateMd5Hash(string input)
         {
-            var md5 = MD5.Create();
-            var inputBytes = Encoding.ASCII.GetBytes(input);
-            var hash = md5.ComputeHash(inputBytes);
-
-            var sb = new StringBuilder();
-            for (var i = 0; i < hash.Length; i++)
+            using (var md5 = MD5.Create())
             {
-                sb.Append(hash[i].ToString("X2"));
-            }
+                var inputBytes = Encoding.ASCII.GetBytes(input);
+                var hash = md5.ComputeHash(inputBytes);
 
-            return sb.ToString();
+                var sb = new StringBuilder();
+
+                for (var i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
         }
     }
 }
