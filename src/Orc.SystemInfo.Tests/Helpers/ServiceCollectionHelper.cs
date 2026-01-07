a6a673e19d38c420f0655047fd10e13a285c82db
Geert van Horrik <geert@catenalogic.com>
2026-01-07 10:53:47 +0100

    Add global initialization for tests

---
+++ src/Orc.SystemInfo.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Orc.SystemInfo.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddOrcSystemInfo();
+
+            return serviceCollection;
+        }
+    }
+}
